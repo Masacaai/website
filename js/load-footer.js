34d15072d832ea8cac26344c6b8d840aabc29fb1
Masacaai <mohammed-shaik@outlook.com>
2025-05-15 13:19:11 -0500

    hardcoded header and footer

--- js/load-footer.js
+++ js/load-footer.js
@@ -1,9 +1,3 @@
-fetch("/components/footer.html")
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById("footer-container").innerHTML = html;
-  })
-
 window.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("ready");
 });
--- js/load-header.js
+++ js/load-header.js
@@ -1,21 +1,15 @@
-fetch("/components/header.html")
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById("header-container").innerHTML = html;
+const path = window.location.pathname;
+let currentLink = null;
+document.querySelectorAll(".topnav a").forEach(link => {
+  if (path.startsWith(link.getAttribute("href"))) {
+    currentLink = link;
+  }
+});
+currentLink.classList.add("active");
 
-    const path = window.location.pathname;
-    let currentLink = null;
-    document.querySelectorAll(".topnav a").forEach(link => {
-      if (path.startsWith(link.getAttribute("href"))) {
-        currentLink = link;
-      }
-    });
-    currentLink.classList.add("active");
+const themeToggle = document.getElementById('theme-toggle');
 
-    const themeToggle = document.getElementById('theme-toggle');
-
-    themeToggle.addEventListener("click", () => {
-      darkmode = localStorage.getItem('darkmode');
-      darkmode !== "active" ? enableDarkmode() : disableDarkmode();
-    });
-  });
+themeToggle.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode');
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
