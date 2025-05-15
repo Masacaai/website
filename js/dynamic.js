e5043ec7b73a0bd59cd95c45866fc1e6438d3e71
Masacaai <mohammed-shaik@outlook.com>
2025-05-15 13:35:51 -0500

    Cleaned up changes

--- js/dynamic.js
+++ js/dynamic.js
@@ -7,9 +7,26 @@ document.querySelectorAll(".topnav a").forEach(link => {
 });
 currentLink.classList.add("active");
 
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkmode', 'active');
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode');
+  localStorage.setItem('darkmode', null);
+};
+
+let darkmode = localStorage.getItem('darkmode');
+if (darkmode === 'active') enableDarkmode();
+
 const themeToggle = document.getElementById('theme-toggle');
 
 themeToggle.addEventListener("click", () => {
   darkmode = localStorage.getItem('darkmode');
   darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("ready");
 });
